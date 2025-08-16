7c3fc91ad7ef75016e076e49973317a0f2aa470a
ErnestoGUFI <159938654+ErnestoGUFI@users.noreply.github.com>
2025-08-15 19:10:49 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+   <View style={styles.container}>
+      <Text style={styles.title}>app react</Text>
+      <Button
+        title="boton"
+        onPress={() => Alert.alert('¡Hola! Este es tu primer botón')}
+      />
     </View>
   );
 }
